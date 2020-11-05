41aea2df824f85578e3490a4a11970f394049827
Paulo Brito <pjbrito@gmail.com>
2020-11-05 12:27:16 +0000

    refactor

--- MovieStudioApi.Tests/AllMovieStudioApiTests.cs
+++ MovieStudioApi.Tests/AllMovieStudioApiTests.cs
@@ -1,18 +1,22 @@
+using MovieStudioApi.DBEntities;
 using NUnit.Framework;
+using System;
 
 namespace MovieStudioApi.Tests
 {
-    public class Tests
+    public class AllMovieStudioApiTests
     {
         [SetUp]
         public void Setup()
         {
         }
 
+
         [Test]
         public void Test1()
         {
             Assert.Pass();
         }
+
     }
 }
