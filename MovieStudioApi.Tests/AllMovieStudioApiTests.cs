e386d46ee308096d5f2de03cac9b440c07e206d7
Paulo Brito <pjbrito@gmail.com>
2020-11-05 12:29:05 +0000

    added Movie entity

--- MovieStudioApi.Tests/AllMovieStudioApiTests.cs
+++ MovieStudioApi.Tests/AllMovieStudioApiTests.cs
@@ -11,6 +11,23 @@ namespace MovieStudioApi.Tests
         {
         }
 
+        public class TheMovieEntityAllows
+        {
+            [Test]
+            public void CreateInstanceWithCorrectValues()
+            {
+                var m = new Movie(1, 2, "Shrek", "EN", "1:30:00", 2001);
+
+                Assert.AreEqual(1, m.Id);
+                Assert.AreEqual(2, m.MovieId);
+                Assert.AreEqual("Shrek", m.Title);
+                Assert.AreEqual("EN", m.Language);
+                Assert.AreEqual(new TimeSpan(1, 30, 0), m.Duration);
+                Assert.AreEqual(2001, m.ReleaseYear);
+            }
+
+        }
+
 
         [Test]
         public void Test1()
---
+++ MovieStudioApi/DBEntities/Movie.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieStudioApi.DBEntities
+{
+    public class Movie
+    {
+
+        public Movie(int id, int movieId, string title, string language, string durationStr, int releaseYear)
+        {
+            this.Id = id;
+            this.MovieId = movieId;
+            this.Title = title;
+            this.Language = language;
+            this.Duration = TimeSpan.ParseExact(durationStr, @"h\:mm\:ss", null, TimeSpanStyles.None);
+            this.ReleaseYear = releaseYear;
+        }
+
+        public int Id { get; set; }
+
+        public int MovieId { get; set; }
+
+        public string Title { get; set; }
+
+        public string Language { get; set; }
+
+        public TimeSpan Duration { get; set; }
+
+        public int ReleaseYear { get; set; }
+
+    }
+
+}
